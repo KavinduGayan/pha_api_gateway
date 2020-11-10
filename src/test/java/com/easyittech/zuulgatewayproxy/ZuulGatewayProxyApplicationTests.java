4b42759fa919ae3af9e9f74439049b0c71717d1b
Kavindu Gayan <gayanrupasinghe21@gmail.com>
2020-11-10 22:24:02 +0530

    first commit

---
+++ src/test/java/com/easyittech/zuulgatewayproxy/ZuulGatewayProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyittech.zuulgatewayproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
